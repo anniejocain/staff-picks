b4aff1814fae095784f765319953afd55e2df3df
Matt Phillips <mphillips@law.harvard.edu>
2013-11-25 12:00:18 -0500

    added settings file

--- README.md
+++ README.md
@@ -3,6 +3,12 @@ Staff picks
 
 
 ## Install Notes
+
+### Configure settings
+Settings are held in etc/settings.ini. Copy that file from the example and edit it with your values.
+    cp settings.example.ini settings.ini
+
+### Configure DB
 Create the DB. If you're using MySQL, you might do something like this:
     create database staff_picks character set utf8;
     grant all on staff_picks.* to staff_picks@'localhost' identified by 'staff_picks';
--- add.php
+++ add.php
@@ -5,6 +5,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if(!empty($_POST['title']) && !empty($_POST['hollis']) && !empty($_POST['selected_by'])) {
         
+        $settings = parse_ini_file("etc/settings.ini", true);
+        print_r($settings);
+        
         $title = $_POST['title'];
         $hollis = $_POST['hollis'];
         $selected_by = $_POST['selected_by'];
@@ -20,12 +23,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     
         // We shold now have all of our values. Let's write them to the DB    
-       $con = mysql_connect("localhost","staff_picks","staff_picks");
+       $con = mysql_connect($settings['MYSQL']['HOST'],$settings['MYSQL']['USER'],$settings['MYSQL']['PASS']);
        if (!$con) {
            die('Could not connect: ' . mysql_error());
        }
    
-       mysql_select_db("staff_picks", $con);
+       mysql_select_db($settings['MYSQL']['DB'], $con);
 
        $sqlCmd = sprintf("INSERT INTO item (title, hollis, isbn, selected_by, cover_path) 
          VALUES ('%s','%s','%s','%s','%s')", 
