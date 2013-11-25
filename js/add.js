35a713fab896257c5ec64c4f4456372bc4186792
Matt Phillips <mphillips@law.harvard.edu>
2013-11-25 14:14:29 -0500

    patched paging on search results

--- js/add.js
+++ js/add.js
@@ -5,12 +5,12 @@ var template = Handlebars.compile(source);
 // Our Library Cloud offset
 var start = 0;
 var num_found = 0;
+var q = '';
 
 var search_lc = function() {
     	// get search term from input
     	// send it to cloud. get results. populate column with results.
     	
-    	var q = $( "input:first" ).val();
     	var url = 'http://librarycloud.harvard.edu/v1/api/item/?filter=keyword:' + q + '&start=' + start + '&limit=5';
 
         $.ajax({
@@ -20,7 +20,7 @@ var search_lc = function() {
     		   $('#results').html(template({results: data['docs']}));
     		   num_found = data.num_found;
     		   $('#result_count').html(num_found.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' matches');
-    		   
+		       button_manager();
     		   $( ".result-item" ).draggable({ revert: "invalid" });
     		   $( ".cover-image" ).each(function( item ) {
 
@@ -29,7 +29,7 @@ var search_lc = function() {
                 $.ajax({
                    url: 'amazon.php?query=' + isbn,
                    dataType: 'json',
-                   success: function(data){	console.log(data[0])
+                   success: function(data){
                          image.attr('src', data[0].thumbnail);
                    }
                 });
@@ -38,7 +38,27 @@ var search_lc = function() {
        });    
 }
 
+var button_manager = function() {
+    console.log('working the buttons. start, num_found.', start, num_found);
+    
+    // Disable/enable prev paging button
+    if (start <= 0) {
+        $("#search-prev").addClass('disabled');
+    } else {
+        $("#search-prev").removeClass('disabled');
+    }    
+
+    // Disable/enable next paging button    
+    if (start >= num_found) {
+        console.log('adding disabled ot next. start, num_found.', start, num_found);
+        $("#search-next").addClass('disabled');
+    } else {
+        $("#search-next").removeClass('disabled');
+    }
+}
+
 $( "#search-form" ).submit(function( event ) {
+	q = $( "input:first" ).val();
     start = 0;
     search_lc();
 	event.preventDefault();
@@ -47,23 +67,25 @@ $( "#search-form" ).submit(function( event ) {
  $( "#drop-box" ).droppable({
     drop: function( event, ui ) {
         $('.result-item').draggable('disable');
-        $( this )
-        .addClass( "ui-state-highlight" ).addClass('full');
+        $( this ).addClass( "ui-state-highlight" ).addClass('full');
     }
 });
 
 $( "#search-prev" ).click(function() {
     start = start - 5;
-
     if (start < 0) {
         start = 0;
-        search_lc();
     }
+    button_manager();
+    search_lc();
 });
 
 $( "#search-next" ).click(function() {
     if (!(start + 5 >= num_found)) {
         start = start + 5;
-        search_lc();
     }
+    button_manager();
+    search_lc();
 });
+
+button_manager();
