775be7a6e4d98711e97282cd69219949e1213279
Matt Phillips <mphillips@law.harvard.edu>
2013-11-25 14:20:58 -0500

    cleaned up add.js. a few comments and some indenting.

--- js/add.js
+++ js/add.js
@@ -2,61 +2,61 @@
 var source   = $('#results-template').html();
 var template = Handlebars.compile(source);
 
-// Our Library Cloud offset
+// Our Library Cloud variables
 var start = 0;
 var num_found = 0;
 var q = '';
 
 var search_lc = function() {
-    	// get search term from input
-    	// send it to cloud. get results. populate column with results.
-    	
-    	var url = 'http://librarycloud.harvard.edu/v1/api/item/?filter=keyword:' + q + '&start=' + start + '&limit=5';
+    // Send our queries to LibraryCloud and write the results to the screen
+    // get search term from input
+    // send it to cloud. get results. populate column with results.
 
-        $.ajax({
-           url: url,
-           dataType: 'jsonp',
-           success: function(data){		   
-    		   $('#results').html(template({results: data['docs']}));
-    		   num_found = data.num_found;
-    		   $('#result_count').html(num_found.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' matches');
-		       button_manager();
-    		   $( ".result-item" ).draggable({ revert: "invalid" });
-    		   $( ".cover-image" ).each(function( item ) {
+    var url = 'http://librarycloud.harvard.edu/v1/api/item/?filter=keyword:' + q + '&start=' + start + '&limit=5';
 
-	            var image = $(this);
-    		    var isbn = $(this).data('isbn');
+    $.ajax({
+        url: url,
+        dataType: 'jsonp',
+        success: function(data){		   
+            $('#results').html(template({results: data['docs']}));
+            num_found = data.num_found;
+            $('#result_count').html(num_found.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' matches');
+            button_manager();
+            $( ".result-item" ).draggable({ revert: "invalid" });
+            $( ".cover-image" ).each(function( item ) {
+                var image = $(this);
+                var isbn = $(this).data('isbn');
                 $.ajax({
-                   url: 'amazon.php?query=' + isbn,
-                   dataType: 'json',
-                   success: function(data){
-                         image.attr('src', data[0].thumbnail);
-                   }
+                    url: 'amazon.php?query=' + isbn,
+                    dataType: 'json',
+                    success: function(data){
+                        image.attr('src', data[0].thumbnail);
+                    }
                 });
-                });
-           }
-       });    
+            });
+        }
+    });    
 }
 
 var button_manager = function() {
-    console.log('working the buttons. start, num_found.', start, num_found);
+    // A helper to disable/enable our paging buttons
     
-    // Disable/enable prev paging button
+    // Prev paging button
     if (start <= 0) {
         $("#search-prev").addClass('disabled');
     } else {
         $("#search-prev").removeClass('disabled');
     }    
 
-    // Disable/enable next paging button    
-    if (start >= num_found) {
-        console.log('adding disabled ot next. start, num_found.', start, num_found);
+    // Next paging button    
+    if ((start + 5 >= num_found)) {
         $("#search-next").addClass('disabled');
     } else {
         $("#search-next").removeClass('disabled');
     }
 }
 
+// When the search form is submitted
 $( "#search-form" ).submit(function( event ) {
 	q = $( "input:first" ).val();
     start = 0;
@@ -64,13 +64,7 @@ $( "#search-form" ).submit(function( event ) {
 	event.preventDefault();
 });
 
- $( "#drop-box" ).droppable({
-    drop: function( event, ui ) {
-        $('.result-item').draggable('disable');
-        $( this ).addClass( "ui-state-highlight" ).addClass('full');
-    }
-});
-
+// When the previous paging button is clicked
 $( "#search-prev" ).click(function() {
     start = start - 5;
     if (start < 0) {
@@ -80,6 +74,7 @@ $( "#search-prev" ).click(function() {
     search_lc();
 });
 
+// When the next paging button is clicked
 $( "#search-next" ).click(function() {
     if (!(start + 5 >= num_found)) {
         start = start + 5;
@@ -88,4 +83,13 @@ $( "#search-next" ).click(function() {
     search_lc();
 });
 
+// When an items is dragged from the the results list to the box
+$( "#drop-box" ).droppable({
+    drop: function( event, ui ) {
+        $('.result-item').draggable('disable');
+        $( this ).addClass( "ui-state-highlight" ).addClass('full');
+    }
+});
+
+// Managing the paging buttons on page load
 button_manager();
