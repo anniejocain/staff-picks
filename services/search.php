5cccea599bdf59e875f8ac410bf6b4a4de2b4d6e
Matt Phillips <mphillips@law.harvard.edu>
2013-11-27 14:37:47 -0500

    cleaned up search service

--- services/search.php
+++ services/search.php
@@ -2,7 +2,7 @@
 
 if (!empty($_GET['q'])) {
     
-    $q = "q=$q";
+    $q = "q=" . urlencode($_GET['q']);
     
     if (preg_match("/^\d{9}/", $_GET['q'])) {
         $q = "q=bib:" . $_GET['q'];
@@ -11,8 +11,8 @@ if (!empty($_GET['q'])) {
     $start = 0;
     $limit = 3;
     
-    $url = "http://webservices.lib.harvard.edu/rest/hollis/search/mods/?$expanded_query";
-    print $url;
+    $url = "http://webservices.lib.harvard.edu/rest/hollis/search/mods/?$q+";
+    
     $agent = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)';
     $ch = curl_init();  
 
@@ -27,13 +27,9 @@ if (!empty($_GET['q'])) {
     $output = curl_exec($ch);
     curl_close($ch);
 
-    //print $output;
     $xml_response = new SimpleXMLElement($output);
 
     $total_results = $xml_response->totalResults[0];
-//    print $total_results;
-  
-//  print_r($xml_response->resultSet);
     
     $to_out = [];
     $to_out['num_found'] = (int) $total_results;
@@ -48,25 +44,14 @@ if (!empty($_GET['q'])) {
         $to_out_item['creator'] = trim(preg_replace('/\s+/', ' ', $raw_creator));
         
         $raw_id_inst = (string) $item->mods->recordInfo[0]->recordIdentifier[0];
+        $raw_id_inst = preg_replace('/-\d$/', ' ', $raw_id_inst);
         $to_out_item['id_inst'] = trim(preg_replace('/\s+/', ' ', $raw_id_inst));
-        
-        //print $item->mods->titleInfo->title[0];
-        //print "\n";
-        //print $item->mods->name[0]->namePart[0];
-        //print "\n";
-        //print $item->mods->recordInfo[0]->recordIdentifier[0];
-        //print "\n";
+
         $docs[] = $to_out_item;
     }
     $to_out['docs'] = $docs;
     
     print json_encode($to_out, JSON_PRETTY_PRINT);
-    
-    
-    //print_r($xml_response->resultSet);
-
-    
-    
 }
 
 ?>
